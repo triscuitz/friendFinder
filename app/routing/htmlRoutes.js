18b66a425ce78a87e1be0afb609f705acd38f512
Nicholas Moore <nmoore@Nicholass-MacBook-Pro.local>
2018-02-18 16:02:21 -0700

    second commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/style.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/style.css"));
+  });
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
